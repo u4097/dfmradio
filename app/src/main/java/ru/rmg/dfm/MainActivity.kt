61df61c1a0a47ff88cfcce33669c98b305a07d37
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2018-12-18 14:08:02 +0300

    fxi main activity

--- app/src/main/java/ru/rmg/dfm/MainActivity.kt
+++ app/src/main/java/ru/rmg/dfm/MainActivity.kt
@@ -4,7 +4,15 @@ import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.list_item.*
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import ru.rmg.dfm.player.PlaybackStatus
 import ru.rmg.dfm.player.RadioManager
+import ru.rmg.dfm.util.Shoutcast
 import ru.rmg.dfm.util.ShoutcastHelper
 import ru.rmg.dfm.util.ShoutcastListAdapter
 
@@ -22,5 +30,86 @@ class MainActivity : AppCompatActivity() {
         radioManager = RadioManager.create(this)
 
         listview.adapter = ShoutcastListAdapter(this, ShoutcastHelper.retrieveShoutcasts(this))
+
+        initView()
     }
+
+    private fun initView() {
+        playTrigger.setOnClickListener {
+            if (TextUtils.isEmpty(streamURL))
+
+                radioManager.playOrPause(streamURL)
+        }
+
+        listview.setOnItemClickListener { parent, view, position, id ->
+
+            val shoutcast = parent.getItemAtPosition(position) as Shoutcast
+
+            name.setText(shoutcast.getName())
+
+            sub_player.setVisibility(View.VISIBLE)
+
+            streamURL = shoutcast.getUrl()!!
+
+            radioManager.playOrPause(streamURL)
+
+        }
+
+    }
+
+    public override fun onStart() {
+
+        super.onStart()
+
+        EventBus.getDefault().register(this)
+    }
+
+    public override fun onStop() {
+
+        EventBus.getDefault().unregister(this)
+
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+
+        radioManager.unbind()
+
+        super.onDestroy()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        radioManager.bind()
+    }
+
+    override fun onBackPressed() {
+
+        finish()
+    }
+
+    @Subscribe
+    fun onEvent(status: String) {
+
+        when (status) {
+
+            PlaybackStatus.LOADING -> {
+            }
+
+            PlaybackStatus.ERROR ->
+
+                Toast.makeText(this, R.string.no_stream, Toast.LENGTH_SHORT).show()
+        }// loading
+
+        playTrigger.setImageResource(
+            if (status == PlaybackStatus.PLAYING)
+                R.drawable.ic_pause_black
+            else
+                R.drawable.ic_play_arrow_black
+        )
+
+    }
+
+
 }
