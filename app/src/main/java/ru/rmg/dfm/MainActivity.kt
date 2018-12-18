60fa38996acf876662c1ccede24fa3a203723b57
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2018-12-18 10:21:30 +0300

    fix toolbar

--- app/src/main/java/ru/rmg/dfm/MainActivity.kt
+++ app/src/main/java/ru/rmg/dfm/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.rmg.dfm
 
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(mainToolbar)
+
     }
 }
